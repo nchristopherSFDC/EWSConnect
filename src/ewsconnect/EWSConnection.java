bd1d7b5d35c6180c9db71b9e1e87b6243c9ac7a3
Nimil <nimil.christopher@makepositive.com>
2012-08-07 09:53:07 +0100

    Rmoved Unused Imports

--- src/ewsconnect/EWSConnection.java
+++ src/ewsconnect/EWSConnection.java
@@ -6,7 +6,6 @@ package ewsconnect;
 
 import com.itextpdf.text.pdf.PdfReader;
 import java.io.File;
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
